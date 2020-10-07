25b6378e59a680b96b3ff3dcf2b27b2934773588
Ellen Spertus <spertus@mills.edu>
2020-10-07 10:13:40 -0700

    Make isPalindrome static

--- src/edu/mills/cs180/PalindromeChecker.java
+++ src/edu/mills/cs180/PalindromeChecker.java
@@ -10,13 +10,12 @@ public class PalindromeChecker {
 
     /**
      * Checks whether the given string is a palindrome. A string is considered a palindrome if it
-     * reads the same forwards and backwards, ignoring case, once all non-alphanumeric characters
-     * are removed.
+     * reads the same forwards and backwards once all non-alphanumeric characters are removed.
      *
      * @param s the string to check
      * @return true if the string is a palindrome, false otherwise
      */
-    public boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         return false;
     }
 }
