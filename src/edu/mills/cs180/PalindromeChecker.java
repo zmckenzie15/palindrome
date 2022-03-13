3036cf696594ff6e036220625b05240c2910248c
zoemckenzie <zmckenzie82@gmail.com>
2022-03-13 14:09:45 -0700

    Implement isPalindrome

--- src/edu/mills/cs180/PalindromeChecker.java
+++ src/edu/mills/cs180/PalindromeChecker.java
@@ -3,7 +3,7 @@ package edu.mills.cs180;
 /**
  * A static utility class for testing whether strings are palindromes.
  *
- * @author Ellen Spertus
+ * @author Zoe McKenzie
  */
 public class PalindromeChecker {
     private PalindromeChecker() {}
@@ -17,16 +17,9 @@ public class PalindromeChecker {
      * @return true if the string is a palindrome, false otherwise
      */
     public static boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(s);
-        sb = sb.reverse();
-        if (s.equals(sb.toString())) {
-            return true;
-        }
-        return false;
+        s = s.replaceAll("[^A-Za-z0-9]", "");
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        return s.equalsIgnoreCase(sb.toString());
     }
-
-    // public static void main(String[] args) {
-    // isPalindrome("abba");
-    // }
 }
